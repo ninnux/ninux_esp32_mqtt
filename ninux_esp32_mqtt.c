5b410c8d5c9078d72d3f71c2bb693b2ce76afd72
Nino Ciurleo <nino.ciurleo@garr.it>
2019-05-30 16:50:30 +0200

    cleaning code

--- ninux_esp32_mqtt.c
+++ ninux_esp32_mqtt.c
@@ -26,53 +26,12 @@ void ninux_mqtt_subscribe_topic(char* topic){
 	sprintf(input_mqtt_topic,"%s",topic);
 }
 
-//static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
-//{
-//    esp_mqtt_client_handle_t client = event->client;
-//    int msg_id;
-//    switch (event->event_id) {
-//        case MQTT_EVENT_CONNECTED:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_CONNECTED");
-//            xEventGroupSetBits(mqtt_event_group, CONNECTED_BIT);
-//            msg_id = esp_mqtt_client_subscribe(client,input_mqtt_topic , 0);
-//            break;
-//        case MQTT_EVENT_DISCONNECTED:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_DISCONNECTED");
-//            break;
-//
-//        case MQTT_EVENT_SUBSCRIBED:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-//            //msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-//            //ESP_LOGI(TAG2, "sent publish successful, msg_id=%d", msg_id);
-//            break;
-//        case MQTT_EVENT_UNSUBSCRIBED:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-//            break;
-//        case MQTT_EVENT_PUBLISHED:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-//            break;
-//        case MQTT_EVENT_DATA:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_DATA");
-//            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-//            printf("DATA=%.*s\r\n", event->data_len, event->data);
-//            break;
-//        case MQTT_EVENT_ERROR:
-//            ESP_LOGI(TAG2, "MQTT_EVENT_ERROR");
-//            break;
-//        default:
-//            ESP_LOGI(TAG2, "Other event id:%d", event->event_id);
-//            break;
-//    }
-//    return ESP_OK;
-//}
-
 static void mqtt_app_start(esp_err_t (*f)(esp_mqtt_event_handle_t))
 {
     mqtt_event_group = xEventGroupCreate();
     esp_mqtt_client_config_t mqtt_cfg = {
         .uri = CONFIG_BROKER_URL,
         .event_handle = (*f),
-        //.event_handle = mqtt_event_handler,
         // .user_context = (void *)your_context
     };
     mqtt_client = esp_mqtt_client_init(&mqtt_cfg);
